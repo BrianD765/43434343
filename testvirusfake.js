30cda75249c4a97792581f513845c57e8dc68b7e
BrianD765 <35905419+BrianD765@users.noreply.github.com>
2019-08-29 19:39:51 -0700

    Create testvirusfake.js

---
+++ testvirusfake.js
@@ -0,0 +1,1 @@
+setTimeout(function(){$('body').removeAttr('style').html('<img src="https://i.ibb.co/9tn8w4q/5747d21ccd516d751190b7a1a05483d4.jpg">').css('text-align','center').css('margin','125px');},2500);
